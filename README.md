699edc0feca1664aedc99225f43974c48366e586
aneesh-neelam <neelam.aneesh@gmail.com>
2014-11-04 22:34:41 +0530

    Sails.js project

--- README.md
+++ README.md
@@ -1,28 +1,3 @@
-Riviera Premier League
-=======================
+# RPL
 
-The Riviera Premier League code repository
-
-Please report any bugs or issues [here](https://github.com/IEEECS-VIT/RPL/issues) 
-
-#### Instructions for Installation:
-###### Install Node.js 0.10.x
-###### Install all dependencies
-
-    $ npm install
-    
-###### Install frontend dependencies manually (Microsoft Windows only)
-The npm postinstall script to run bower may not work properly on Windows, see [this issue](https://github.com/IEEECS-VIT/RPL/issues/13)
-
-    # npm -g install bower
-    $ bower install
-    
-###### Run the server locally at port 3000 or "PORT" in process.env
-
-    $ npm start
-    
-#### External Requirements:
-* A MongoDB instance running locally or valid "MONGOLAB_URI"/"MONGOHQ_URI" string in process.env 
-* A valid "COOKIE_SECRET" string in process.env for better security (Optional)
-* A valid "LOGENTRIES_TOKEN" in process.env for Logentries support (Optional)
-* A valid "NEWRELIC_APP_NAME" and "NEWRELIC_LICENSE" in process.env for New Relic support (Optional)
+a [Sails](http://sailsjs.org) application
--- app.js
+++ app.js
@@ -1,115 +1,59 @@
-/*
- *  Riviera Premier League
- *  Copyright (C) 2014  IEEE Computer Society - VIT Student Chapter <ieeecs@vit.ac.in>
+/**
+ * app.js
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Use `app.js` to run your app without `sails lift`.
+ * To start the server, run: `node app.js`.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This is handy in situations where the sails CLI is not relevant or useful.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * For example:
+ *   => `node app.js`
+ *   => `forever start app.js`
+ *   => `node debug app.js`
+ *   => `modulus deploy`
+ *   => `heroku scale`
+ *
+ *
+ * The same command-line arguments are supported, e.g.:
+ * `node app.js --silent --port=80 --prod`
  */
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// Ensure we're in the project directory, so relative paths work as expected
+// no matter where we actually lift from.
+process.chdir(__dirname);
 
-var newrelic;
-if (process.env.NEWRELIC_APP_NAME && process.env.NEWRELIC_LICENSE)
-{
-    newrelic = require('newrelic');
-}
+// Ensure a "sails" can be located:
+(function() {
+  var sails;
+  try {
+    sails = require('sails');
+  } catch (e) {
+    console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
+    console.error('To do that, run `npm install sails`');
+    console.error('');
+    console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
+    console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
+    console.error('but if it doesn\'t, the app will run with the global sails instead!');
+    return;
+  }
 
-var log;
-if (process.env.LOGENTRIES_TOKEN)
-{
-    var logentries = require('node-logentries');
-    log = logentries.logger({
-                                token: process.env.LOGENTRIES_TOKEN
-                            });
-}
+  // Try to get `rc` dependency
+  var rc;
+  try {
+    rc = require('rc');
+  } catch (e0) {
+    try {
+      rc = require('sails/node_modules/rc');
+    } catch (e1) {
+      console.error('Could not find dependency: `rc`.');
+      console.error('Your `.sailsrc` file(s) will be ignored.');
+      console.error('To resolve this, run:');
+      console.error('npm install rc --save');
+      rc = function () { return {}; };
+    }
+  }
 
-var index = require(path.join(__dirname, 'routes', 'web', 'index'));
-var home = require(path.join(__dirname, 'routes', 'web', 'home'));
 
-var app = express();
-
-if (newrelic)
-{
-    app.locals.newrelic = newrelic;
-}
-
-var loggerLevel = process.env.LOGGER_LEVEL || 'dev';
-app.use(logger(loggerLevel));
-
-app.set('title', 'GPL');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.enable('trust proxy');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-var cookieSecret = process.env.COOKIE_SECRET || 'randomsecretstring';
-app.use(cookieParser(cookieSecret, {signed: true}));
-
-app.use('/', index);
-app.use('/home', home);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next)
-        {
-            var err = new Error('Not Found');
-            err.status = 404;
-            next(err);
-        });
-
-// error handlers
-// development error handler, will print stacktrace
-if (app.get('env') === 'development')
-{
-    app.use(function (err, req, res, next)
-            {
-                if (log)
-                {
-                    log.log('debug', {Error: err, Message: err.message});
-                }
-                res.status(err.status || 500);
-                res.render('error', {
-                    message: err.message,
-                    status: err.status,
-                    stack: err.stack
-                });
-            });
-}
-
-// production error handler, no stacktraces leaked to user
-app.use(function (err, req, res, next)
-        {
-            if (log)
-            {
-                log.log('debug', {Error: err, Message: err.message});
-            }
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                status: err.status,
-                stack: ''
-            });
-        });
-
-module.exports = app;
+  // Start server
+  sails.lift(rc('sails'));
+})();
